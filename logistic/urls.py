0fe2911ef6cf66d32413c7d104175252f0702f50
Ilya Voloshin <ilyavoloshin20@gmail.com>
2023-05-14 19:30:21 +0700

    cicd280

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from logistic.views import ProductViewSet, StockViewSet
+from logistic.views import ProductViewSet, StockViewSet, test_change
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
-# router.register('api/v1/test/', test_change)
+router.register('test', test_change)
 
 
 urlpatterns = router.urls
--- logistic/views.py
+++ logistic/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
-# from rest_framework.response import Response
+from rest_framework.response import Response
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -21,5 +21,5 @@ class StockViewSet(ModelViewSet):
     filterset_fields = ['products']
 
 
-# def test_change():
-#     return Response("Всем привет! Все получилось!")
+def test_change():
+    return Response("Всем привет! Все получилось!")
