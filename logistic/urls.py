c347ee0691425252f88fcb6f06c874ffe95a1116
Ilya Voloshin <ilyavoloshin20@gmail.com>
2023-05-14 19:17:20 +0700

    cicd26

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path
 
 from logistic.views import ProductViewSet, StockViewSet, test_change
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
+router.register('api/v1/test/', test_change)
 
 
-urlpatterns = router.urls, path('api/v1/test/', test_change)
+urlpatterns = router.urls
