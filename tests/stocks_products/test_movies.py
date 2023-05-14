cb7a93a8b8ba4555242a4f50980b0089149a019f
Ilya Voloshin <ilyavoloshin20@gmail.com>
2023-05-14 19:55:57 +0700

    cicd399

--- tests/stocks_products/test_movies.py
+++ tests/stocks_products/test_movies.py
@@ -6,23 +6,23 @@ from rest_framework.test import APIClient
 def client():
     return APIClient()
 
-# @pytest.mark.django_db
-# def test_get_stock(client):
-#     response = client.get('/api/v1/stocks/')
-#     assert response.status_code == 200
-
-# @pytest.mark.django_db
-# def test_create_product(client):
-#     response = client.post('/api/v1/products/',{
-#                                                 "title": "Помидор",
-#                                                 "description": "Лучшие помидоры на рынке"
-#                                                 })
-#     assert response.status_code == 201
+@pytest.mark.django_db
+def test_get_stock(client):
+    response = client.get('/api/v1/stocks/')
+    assert response.status_code == 200
 
 @pytest.mark.django_db
-def control_test(client):
-    response = client.get('/api/v1/test/')
-    assert response.data == "Всем привет! Все получилось!"
+def test_create_product(client):
+    response = client.post('/api/v1/products/',{
+                                                "title": "Помидор",
+                                                "description": "Лучшие помидоры на рынке"
+                                                })
+    assert response.status_code == 201
+
+# @pytest.mark.django_db
+# def control_test(client):
+#     response = client.get('/api/v1/test/')
+#     assert response.data == "Всем привет! Все получилось!"
 
 
 
